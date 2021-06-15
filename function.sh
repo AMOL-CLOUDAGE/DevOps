b9e854ad7cfee8a16aed627ee42c2c373b8af933
amol <harale014@gmail.com>
2021-06-15 11:47:10 +0530

    7

--- function.sh
+++ function.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Declare a function
+
 SAMPLE() {
-  echo wlcome to sample function
+  echo Welcome to SAMPLE Function
+  return 5
+  echo Value of a = $a
+  b=20
+  echo First Argument = $1
 }
