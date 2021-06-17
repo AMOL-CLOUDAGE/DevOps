cec7cd0e252d105c08355c917a33c4c7ecf04312
amol <harale014@gmail.com>
2021-06-17 09:03:55 +0530

    8

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -7,8 +7,3 @@ set-hostname frontend
 HEAD "Installing Nginx\t"
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
-
-HEAD "Download from GitHub\t"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
-STAT $?
-
